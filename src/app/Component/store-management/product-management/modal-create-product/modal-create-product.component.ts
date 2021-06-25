f8507a217c9a54dce916cdc84955fc292ae7f4fc
Triết <triettran@meu-solutions.com>
2021-06-25 21:36:07 +0700

    Fix bug

--- src/app/Component/store-management/product-management/modal-create-product/modal-create-product.component.ts
+++ src/app/Component/store-management/product-management/modal-create-product/modal-create-product.component.ts
@@ -15,7 +15,6 @@ import { environment } from 'src/environments/environment';
 export class ModalCreateProductComponent implements OnInit {
   @Input() id_product: number;
   @Input() option: boolean = true;
-  @Input() title: string;
   @Output() emitModal = new EventEmitter();
 
   id_update: number;
@@ -120,34 +119,34 @@ export class ModalCreateProductComponent implements OnInit {
               }
             });
           if (this.urlUpdate1) {
-            const formData: FormData = new FormData();
-            formData.append('file', this.urlUpdate1, this.urlUpdate1.name);
+            const formData1: FormData = new FormData();
+            formData1.append('file', this.urlUpdate1, this.urlUpdate1.name);
             this.productService
-              .updateImageProduct1(res.idProduct, formData)
-              .subscribe((res1: any) => {
-                if (res1.success == true) {
+              .updateImageProduct1(res.idProduct, formData1)
+              .subscribe((res4: any) => {
+                if (res4.success == true) {
                 } else {
                 }
               });
           }
           if (this.urlUpdate2) {
-            const formData: FormData = new FormData();
-            formData.append('file', this.urlUpdate2, this.urlUpdate2.name);
+            const formData2: FormData = new FormData();
+            formData2.append('file', this.urlUpdate2, this.urlUpdate2.name);
             this.productService
-              .updateImageProduct2(res.idProduct, formData)
-              .subscribe((res1: any) => {
-                if (res1.success == true) {
+              .updateImageProduct2(res.idProduct, formData2)
+              .subscribe((res2: any) => {
+                if (res2.success == true) {
                 } else {
                 }
               });
           }
           if (this.urlUpdate3) {
-            const formData: FormData = new FormData();
-            formData.append('file', this.urlUpdate3, this.urlUpdate3.name);
+            const formData3: FormData = new FormData();
+            formData3.append('file', this.urlUpdate3, this.urlUpdate3.name);
             this.productService
-              .updateImageProduct3(res.idProduct, formData)
-              .subscribe((res1: any) => {
-                if (res1.success == true) {
+              .updateImageProduct3(res.idProduct, formData3)
+              .subscribe((res3: any) => {
+                if (res3.success == true) {
                 } else {
                 }
               });
@@ -169,75 +168,129 @@ export class ModalCreateProductComponent implements OnInit {
     this.productService
       .updateProductID(this.product.idProduct, this.product)
       .subscribe((res: any) => {
-        if (res.message == true) {
+        console.log(res);
+        if (res) {
+          if (this.urlUpdate) {
+            var formData: FormData = new FormData();
+            formData.append('file', this.urlUpdate, this.urlUpdate.name);
+            this.productService
+              .updateImageProduct(this.id_product, formData)
+              .subscribe((res: any) => {
+                if (res.success == true) {
+                  console.log(1);
+                } else {
+                }
+              });
+          }
+          if (this.urlUpdate1) {
+            var formData1: FormData = new FormData();
+            formData1.append('file', this.urlUpdate1, this.urlUpdate1.name);
+            this.productService
+              .updateImageProduct1(this.id_product, formData1)
+              .subscribe((res1: any) => {
+                if (res1.success == true) {
+                  console.log(2);
+                } else {
+                }
+              });
+          }
+          if (this.urlUpdate2) {
+            var formData2: FormData = new FormData();
+            formData2.append('file', this.urlUpdate2, this.urlUpdate2.name);
+            this.productService
+              .updateImageProduct2(this.id_product, formData2)
+              .subscribe((res2: any) => {
+                if (res2.success == true) {
+                  console.log(3);
+                } else {
+                }
+              });
+          }
+          if (this.urlUpdate3) {
+            var formData3: FormData = new FormData();
+            formData3.append('file', this.urlUpdate3, this.urlUpdate3.name);
+            this.productService
+              .updateImageProduct3(this.id_product, formData3)
+              .subscribe((res3: any) => {
+                if (res3.success == true) {
+                  console.log(4);
+                } else {
+                }
+              });
+          }
         }
-        this.toastr.success(res.message);
-        this.edmitModal(res);
+        this.toastr.success('Thao tác thành công');
+        this.edmitModal();
       });
   }
-  upImage() {
-    var check: boolean;
-    if (this.urlUpdate) {
-      const formData: FormData = new FormData();
-      formData.append('file', this.urlUpdate, this.urlUpdate.name);
-      this.productService
-        .updateImageProduct(this.id_product, formData)
-        .subscribe((res: any) => {
-          if (res.success == true) {
-            check = true;
-          } else {
-            check = false;
-          }
-        });
-    }
-    if (this.urlUpdate1) {
-      const formData: FormData = new FormData();
-      formData.append('file', this.urlUpdate1, this.urlUpdate1.name);
-      this.productService
-        .updateImageProduct1(this.id_product, formData)
-        .subscribe((res: any) => {
-          if (res.success == true) {
-            check = true;
-          } else {
-            check = false;
-          }
-        });
-    }
-    if (this.urlUpdate2) {
-      const formData: FormData = new FormData();
-      formData.append('file', this.urlUpdate2, this.urlUpdate2.name);
-      this.productService
-        .updateImageProduct2(this.id_product, formData)
-        .subscribe((res: any) => {
-          if (res.success == true) {
-            check = true;
-          } else {
-            check = false;
-          }
-        });
-    }
-    if (this.urlUpdate3) {
-      const formData: FormData = new FormData();
-      formData.append('file', this.urlUpdate3, this.urlUpdate3.name);
-      this.productService
-        .updateImageProduct3(this.id_product, formData)
-        .subscribe((res: any) => {
-          if (res.success == true) {
-            check = true;
-          } else {
-            check = false;
-          }
-        });
-    }
-    if (
-      this.urlUpdate3 ||
-      this.urlUpdate2 ||
-      this.urlUpdate1 ||
-      this.urlUpdate
-    ) {
-      this.toastr.success('Upload hình ảnh thành công');
-    }
-  }
+  // upImage() {
+  //   var check: boolean;
+  //   if (this.urlUpdate) {
+  //     var formData: FormData = new FormData();
+  //     formData.append('file', this.urlUpdate, this.urlUpdate.name);
+  //     this.productService
+  //       .updateImageProduct(this.id_product, formData)
+  //       .subscribe((res: any) => {
+  //         if (res.success == true) {
+  //           console.log(1);
+
+  //           check = true;
+  //         } else {
+  //           check = false;
+  //         }
+  //       });
+  //   }
+  //   if (this.urlUpdate1) {
+  //     var formData1: FormData = new FormData();
+  //     formData1.append('file', this.urlUpdate1, this.urlUpdate1.name);
+  //     this.productService
+  //       .updateImageProduct1(this.id_product, formData1)
+  //       .subscribe((res: any) => {
+  //         if (res.success == true) {
+  //           console.log(2);
+  //           check = true;
+  //         } else {
+  //           check = false;
+  //         }
+  //       });
+  //   }
+  //   if (this.urlUpdate2) {
+  //     var formData2: FormData = new FormData();
+  //     formData2.append('file', this.urlUpdate2, this.urlUpdate2.name);
+  //     this.productService
+  //       .updateImageProduct2(this.id_product, formData2)
+  //       .subscribe((res: any) => {
+  //         if (res.success == true) {
+  //           check = true;
+  //           console.log(3);
+  //         } else {
+  //           check = false;
+  //         }
+  //       });
+  //   }
+  //   if (this.urlUpdate3) {
+  //     var formData3: FormData = new FormData();
+  //     formData3.append('file', this.urlUpdate3, this.urlUpdate3.name);
+  //     this.productService
+  //       .updateImageProduct3(this.id_product, formData3)
+  //       .subscribe((res: any) => {
+  //         if (res.success == true) {
+  //           console.log(4);
+  //           check = true;
+  //         } else {
+  //           check = false;
+  //         }
+  //       });
+  //   }
+  //   if (
+  //     this.urlUpdate3 ||
+  //     this.urlUpdate2 ||
+  //     this.urlUpdate1 ||
+  //     this.urlUpdate
+  //   ) {
+  //     this.toastr.success('Upload hình ảnh thành công');
+  //   }
+  // }
 
   //Emit data
   edmitModal(data = null) {
@@ -259,7 +312,7 @@ export class ModalCreateProductComponent implements OnInit {
 
   //Get image
   fileChangeEvent(event) {
-    this.urlUpdate = event.item;
+    console.log(event.target.files[0]);
 
     if (event.target.files) {
       var reader = new FileReader();
@@ -271,8 +324,6 @@ export class ModalCreateProductComponent implements OnInit {
     }
   }
   fileChangeEvent1(event) {
-    this.urlUpdate1 = event.item;
-
     if (event.target.files) {
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
@@ -283,8 +334,6 @@ export class ModalCreateProductComponent implements OnInit {
     }
   }
   fileChangeEvent2(event) {
-    this.urlUpdate2 = event.item;
-
     if (event.target.files) {
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
@@ -295,8 +344,6 @@ export class ModalCreateProductComponent implements OnInit {
     }
   }
   fileChangeEvent3(event) {
-    this.urlUpdate3 = event.item;
-
     if (event.target.files) {
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
